e5ff6dd72e24eac76001ecf159fc85824e627593
UlongChaS2 <jyujung2@gmail.com>
2021-09-09 23:19:55 +0900

    Resolve2: 20210910 K번째수

--- programmers/K번째수.js
+++ programmers/K번째수.js
@@ -1,14 +1,14 @@
 function solution(array, commands) {
   let answer = [];
 
-  for (let i = 0; i < commands.length; i++) {
-    const newArr = array
-      .slice(commands[i][0] - 1, commands[i][1])
-      .sort((a, b) => a - b);
-    // sort()의 콜백함수로 정렬 순서를 정의하는 함수를 생략하면 문자열 변환에 따라 각 문자의 유니 코드 코드 포인트 값에 따라 정렬된다.
-    console.log(newArr);
-    answer.push(newArr[commands[i][2] - 1]);
-  }
+  commands.map((i) => {
+    answer.push(
+      array
+        .slice(i[0] - 1, i[1])
+        .sort((a, b) => a - b)
+        .slice(i[2] - 1, i[2])[0]
+    );
+  });
   return answer;
 }
 
