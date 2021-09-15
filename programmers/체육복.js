bca2d38cd07702e682f54877f7683acae2c2393f
UlongChaS2 <jyujung2@gmail.com>
2021-09-15 23:49:25 +0900

    Consider: 문제풀이 전 체육복

---
+++ programmers/체육복.js
@@ -0,0 +1,16 @@
+function solution(n, lost, reserve) {
+  let answer = n;
+  const realLost = lost.filter((element) => !reserve.includes(element));
+  let realReserve = reserve.filter((element) => !lost.includes(element));
+
+  realReserve.forEach((p) => {
+    let key = realLost.includes(p - 1)
+      ? realLost.indexOf(p - 1)
+      : realLost.indexOf(p + 1);
+    if (key != -1) {
+      realLost.splice(key, 1);
+    }
+  });
+
+  return answer - realLost.length;
+}
