f12e64404ed7ad02bc4029e20b7cd9e83e802bd5
UlongChaS2 <jyujung2@gmail.com>
2021-10-28 19:30:58 +0900

    Resolve: 짐나르기

--- programmers/체육복.js
+++ programmers/체육복.js
@@ -3,11 +3,19 @@ function solution(n, lost, reserve) {
   const realLost = lost.filter((element) => !reserve.includes(element));
   let realReserve = reserve.filter((element) => !lost.includes(element));
 
+  console.log(realLost, realReserve);
+
   realReserve.forEach((p) => {
-    let key = realLost.includes(p - 1)
-      ? realLost.indexOf(p - 1)
-      : realLost.indexOf(p + 1);
-    if (key != -1) {
+    let key;
+    if (p !== 1) {
+      key = realLost.includes(p - 1)
+        ? realLost.indexOf(p - 1)
+        : realLost.indexOf(p + 1);
+      if (key != -1) {
+        realLost.splice(key, 1);
+      }
+    } else {
+      key = realLost.indexOf(p + 1);
       realLost.splice(key, 1);
     }
   });
