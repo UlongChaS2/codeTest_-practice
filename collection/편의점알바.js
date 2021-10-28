70c69e567cd5e27eaaaf2ba388212ec8ff723c04
UlongChaS2 <jyujung2@gmail.com>
2021-10-28 20:00:41 +0900

    Reslove: 편의점알바

--- collection/편의점알바.js
+++ collection/편의점알바.js
@@ -2,3 +2,18 @@
 // 현재 가지고 있는 동전은 1원, 5원, 10원, 50원, 100원, 500원으로 오름차순으로 정렬되어 있고,
 // 각 동전들은 서로 배수 관계에 있습니다.
 // 동전 개수를 최소화하여 거스름돈 K를 만들어야 합니다. 이때, 필요한 동전 개수의 최솟값을 구하는 함수를 작성해 주세요.
+
+function solution(K) {
+  const coin = [500, 100, 50, 10, 5, 1];
+  let count = 0;
+
+  for (let i = 0; i < coin.length; i++) {
+    let test = Math.floor(K / coin[i]);
+    K -= coin[i] * test;
+    count += test;
+  }
+  return count;
+}
+
+const solution1 = solution(1100);
+const solution2 = solution(5231);
